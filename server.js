279675ea6501d57d770c02c49a8f61a11965a654
shivamgpt08 <36909868+shivamgpt08@users.noreply.github.com>
2020-04-12 18:01:40 +0530

    add compression js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const  express = require("express");
+const compression = require("compression");
 const  app = express();
+
+// compress all responses
+app.use(compression())
 
 app.use(express.static(__dirname + "/dist"));
 
